dda76ca3e80de472adbe9b5e432d6f9efc4d6197
Amir <amir.nagri+git@gmail.com>
2016-01-31 19:21:48 +0700

    removing currency converter impl

--- app/src/main/java/co/creativev/bootcamp/got/CurrencyConverter.java
+++
@@ -1,9 +0,0 @@
-package co.creativev.bootcamp.got;
-
-public class CurrencyConverter {
-    public double convert(int amount, String currency) {
-        if (currency.equals("INR"))
-            return amount * 200.0;
-        return amount / 200.0;
-    }
-}
