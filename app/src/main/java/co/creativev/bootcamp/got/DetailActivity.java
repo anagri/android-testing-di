b6e8e99b80fee66f4ff880bc10fa9ed8c3f17f21
Amir <amir.nagri+git@gmail.com>
2016-01-31 18:59:26 +0700

    fixing the name of variable for smooth refactoring

--- app/src/main/java/co/creativev/bootcamp/got/DetailActivity.java
+++ app/src/main/java/co/creativev/bootcamp/got/DetailActivity.java
@@ -15,6 +15,7 @@ public class DetailActivity extends AppCompatActivity {
 
     public static final String EXTRA_CHARACTER = "character";
 
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
