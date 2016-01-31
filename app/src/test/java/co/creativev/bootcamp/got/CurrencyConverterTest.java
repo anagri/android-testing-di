bed1a70690ca7e194b723b54cdbb5acde24b6bed
Amir <amir.nagri+git@gmail.com>
2016-01-31 17:16:30 +0700

    hamcrest

--- app/src/test/java/co/creativev/bootcamp/got/CurrencyConverterTest.java
+++ app/src/test/java/co/creativev/bootcamp/got/CurrencyConverterTest.java
@@ -2,34 +2,35 @@ package co.creativev.bootcamp.got;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CurrencyConverterTest {
     @Test
     public void testConvertsFrom1INRTo200IDR() throws Exception {
         CurrencyConverter converter = new CurrencyConverter();
         double idr = converter.convert(1, "INR");
-        assertEquals(0.001, 200d, idr);
+        assertThat(idr, is(200d));
     }
 
     @Test
     public void testConverts100INRTo20000IDR() throws Exception {
         CurrencyConverter converter = new CurrencyConverter();
         double idr = converter.convert(100, "INR");
-        assertEquals(0.001, 20000d, idr);
+        assertThat(idr, is(20000d));
     }
 
     @Test
     public void testConverts200IDRTo1INR() throws Exception {
         CurrencyConverter converter = new CurrencyConverter();
         double inr = converter.convert(200, "IDR");
-        assertEquals(0.001, 1, inr);
+        assertThat(inr, is(1d));
     }
 
     @Test
     public void testConverts100IDRTo0_5INR() throws Exception {
         CurrencyConverter converter = new CurrencyConverter();
-        double inr = converter.convert(100, "INR");
-        assertEquals(0.001, 0.5, inr);
+        double inr = converter.convert(100, "IDR");
+        assertThat(inr, is(0.5d));
     }
 }
